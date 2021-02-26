6cef19691989d0bca798ddf782f5a52218a9aa0e
Git-Lior <liorkletter@gmail.com>
2021-02-26 11:43:37 +0200

    admin panel - added send mail to new user with temp password

--- client/src/admin/index.tsx
+++ client/src/admin/index.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Dialog from "@material-ui/core/Dialog";
+import Typography from "@material-ui/core/Typography";
 
 import { useCRUDApi } from "utils/hooks";
 
 import AuthPanel from "./AuthPanel";
 import UsersTable from "./UsersTable";
 import GroupsTable from "./GroupsTable";
-import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+type UserDataWithTempPassword = orch.OptionalId<orch.UserData> & { temporaryPassword?: string };
 
 export default function AdminApp() {
   const [adminToken, setAdminToken] = useState<string>();
@@ -34,6 +38,10 @@ const useStyles = makeStyles({
       "&:not(:last-child)": { marginRight: "2em" },
     },
   },
+  dialog: { padding: "3rem", display: "flex", flexDirection: "column", alignItems: "center" },
+  dialogTitle: { marginBottom: "2rem" },
+  dialogPart: { width: "100%", marginBottom: "1rem" },
+  dialogMailButton: { marginTop: "1rem" },
 });
 
 interface ContentProps {
@@ -42,6 +50,7 @@ interface ContentProps {
 
 function AdminAppContent({ token }: ContentProps) {
   const classes = useStyles();
+  const [createdUser, setCreatedUser] = useState<Required<UserDataWithTempPassword>>();
   const [users, refreshUsers, changeUser, deleteUser] = useCRUDApi<orch.UserData>(token, "/users");
   const [groups, refreshGroups, changeGroup, deleteGroup] = useCRUDApi<
     orch.GroupData,
@@ -56,13 +65,27 @@ function AdminAppContent({ token }: ContentProps) {
     })();
   }, [refreshUsers, refreshGroups]);
 
+  const onUserChange = useCallback(
+    async (user: orch.OptionalId<orch.UserData>) => {
+      const result: UserDataWithTempPassword = await changeUser(user);
+      if (result.id && result.temporaryPassword) setCreatedUser(result as any);
+    },
+    [changeUser]
+  );
+
+  const clearCreatedUser = useCallback(() => setCreatedUser(undefined), []);
+
+  const sendInviteMail = useCallback(() => window.open(getMailHref(createdUser), "_blank"), [
+    createdUser,
+  ]);
+
   return (
     <div className={classes.adminPanel}>
       <Typography variant="h4" color="primary" className={classes.panelTitle}>
         Admin Panel
       </Typography>
       <div className={classes.tables}>
-        <UsersTable users={users} onUserChange={changeUser} onUserDelete={deleteUser} />
+        <UsersTable users={users} onUserChange={onUserChange} onUserDelete={deleteUser} />
         <GroupsTable
           groups={groups}
           users={users}
@@ -70,6 +93,41 @@ function AdminAppContent({ token }: ContentProps) {
           onGroupDelete={deleteGroup}
         />
       </div>
+      <Dialog open={!!createdUser} onClose={clearCreatedUser}>
+        <div className={classes.dialog}>
+          <Typography variant="h4" color="primary" className={classes.dialogTitle}>
+            New User Created
+          </Typography>
+          <div className={classes.dialogPart}>
+            <Typography variant="body1">First Name: {createdUser?.firstName}</Typography>
+            <Typography variant="body1">Last Name: {createdUser?.lastName}</Typography>
+          </div>
+          <div className={classes.dialogPart}>
+            <Typography variant="h6">Email: {createdUser?.email}</Typography>
+            <Typography variant="h6">
+              Temporary Password: {createdUser?.temporaryPassword}
+            </Typography>
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.dialogMailButton}
+            onClick={sendInviteMail}
+          >
+            Invite with email
+          </Button>
+        </div>
+      </Dialog>
     </div>
   );
+}
+
+function getMailHref(user?: UserDataWithTempPassword): string {
+  if (!user) return "";
+
+  const subject = `Hey ${user.firstName}, welcome to Orchestrate!`;
+
+  const body = `join us at: ${document.location.origin} \nyour temporary password is: ${user.temporaryPassword}`;
+
+  return encodeURI(`mailto:${user.email}?subject=${subject}&body=${body}`);
 }
